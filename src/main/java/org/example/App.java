b7d41de4a6c204eb89a6b110f801491ce2f68f4e
RVSN451 <kvg.huawei@gmail.com>
2021-11-30 12:24:56 +0300

    LoanCalculator.Class added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,18 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.math.BigDecimal;
+
+public class App {
+
+    public static void main(String[] args) {
+        LoanCalculator calculator = new LoanCalculator(
+                new BigDecimal(2_000_000),
+                36,
+                new BigDecimal(12)
+        );
+
+        System.out.println("Ежемесячный платёж - " + calculator.monthlyPayment() +
+                "\nОбщая сумма к возврату в банк - " + calculator.totalAmountReturnedToBank() +
+                "\nПереплата за весь период - " + calculator.overpaymentCalculationEntirePeriod());
     }
 }
--- src/main/java/org/example/LoanCalculator.java
+++ src/main/java/org/example/LoanCalculator.java
@@ -6,6 +6,7 @@ import lombok.NonNull;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.math.RoundingMode;
 
 @Data
 @AllArgsConstructor
@@ -21,15 +22,35 @@ public class LoanCalculator {
     private BigDecimal annualInterestRate;
 
     public BigDecimal monthlyPayment() {
-        return new BigDecimal(1);
+
+        return creditAmount.multiply(
+                monthlyInterestRate().divide(
+                        new BigDecimal(1).subtract(
+                                negativeDegree(new BigDecimal(1).add(monthlyInterestRate()),
+                                        creditTermMonth)
+                        ), bigDecimalContext
+                )
+        );
 
     }
 
     public BigDecimal totalAmountReturnedToBank() {
-        return new BigDecimal(1);
+        return monthlyPayment().multiply(BigDecimal.valueOf(creditTermMonth));
     }
 
     public BigDecimal overpaymentCalculationEntirePeriod() {
-        return new BigDecimal(1);
+        return totalAmountReturnedToBank().subtract(creditAmount);
+
+    }
+
+    private BigDecimal monthlyInterestRate() {
+        return annualInterestRate.divide((monthsInYear.multiply(new BigDecimal(100))),
+                5, RoundingMode.CEILING);
+    }
+
+    private BigDecimal negativeDegree(BigDecimal a, int degree) {
+
+        return new BigDecimal(1).divide(a.pow(degree), bigDecimalContext);
+
     }
 }
